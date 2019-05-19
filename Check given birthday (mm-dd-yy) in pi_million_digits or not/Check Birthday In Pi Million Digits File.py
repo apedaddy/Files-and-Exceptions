455b3a43f50e819bb1a78ea31b55448838e254a8
apedaddy <ganesh.baral@hotmail.co.uk>
2019-05-19 12:57:37 +0100

    Check if birthday appear in the value of pi with million decimal values or not Also count number of each digits in the decimal values.

---
+++ Check given birthday (mm-dd-yy) in pi_million_digits or not/Check Birthday In Pi Million Digits File.py
@@ -0,0 +1,23 @@
+my_file = ('pi_million_digits.txt')
+
+with open(my_file) as file_object:
+    lines = file_object.readlines()
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+''' CHECK IF BIRTHDAY APPEAR IN PI_DIGITS OR NOT'''
+birthday = input("Please enter your birthday in (MMDDYY) format: ")
+
+if birthday in pi_string:
+    print ("Your birthday appear in pi decimal digits")
+else:
+    print("Your birthday didn't appear in pi decimal digits")
+
+''' CHECK WHICH NUMBER APPEAR MOST IN THE PI_MILLION DIGITS'''
+print()
+for checker in range (0,10):
+    count_digits = pi_string.count(str(checker))
+    print(f"{checker} : Appear {count_digits} times in PI Million Decimal points")
+
+
